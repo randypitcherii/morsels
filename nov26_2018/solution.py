99b6159a5edaca4b7a9af063528d338d9f4adf13
Randy Pitcher <randypitcherii@gmail.com>
2018-11-26 13:19:53 -0600

    implemented passing first draft solution

--- nov26_2018/solution.py
+++ nov26_2018/solution.py
@@ -1,2 +1,8 @@
 def add(matrixA, matrixB):
-    return matrixA
+    result = []
+    for rowA, rowB in zip(matrixA, matrixB):
+        result.append([])
+        for entryA, entryB in zip(rowA, rowB):
+            result[-1].append(entryA + entryB)
+    
+    return result
