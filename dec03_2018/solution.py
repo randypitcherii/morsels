6dae857b162728775d558b3325521e983f0061d1
Randy Pitcher <randypitcherii@gmail.com>
2018-12-04 14:42:00 -0600

    beginning progress with property annotations

--- dec03_2018/solution.py
+++ dec03_2018/solution.py
@@ -1,7 +1,15 @@
 import math
 
 class Circle():
+    @property
+    def radius(self):
+        return self.radius
+    
+    @radius.setter
+    def radius(self, newRadius):
+        self.radius = newRadius
+        self.diameter = 2*newRadius
+        self.area = math.pi * newRadius**2
+
     def __init__(self, initialRadius=1):
         self.radius = initialRadius
-        self.diameter = 2 * self.radius
-        self.area = math.pi * self.radius ** 2
--- dec03_2018/test.py
+++ dec03_2018/test.py
@@ -15,5 +15,18 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(2, c.diameter)
         self.assertAlmostEqual(math.pi, c.area)
 
+    def test_RadiusChange(self):
+        # create circle and check valid values
+        c = Circle()
+        self.assertEqual(1, c.radius)
+        self.assertEqual(2, c.diameter)
+        self.assertAlmostEqual(math.pi, c.area)
+
+        # update radius and check valid values
+        c.radius = 3
+        self.assertEqual(3, c.radius)
+        self.assertEqual(6, c.diameter)
+        self.assertAlmostEqual(math.pi * 3**2, c.area)
+
 if __name__ == '__main__':
     unittest.main()
