3776786844336f9c66963431a9d5275346fc11a5
Randy Pitcher <randypitcherii@gmail.com>
2019-12-23 11:05:09 -0600

    added support for negative weekday occurence indices

--- 2019-12-23--meetup_date/meetup_date.py
+++ 2019-12-23--meetup_date/meetup_date.py
@@ -21,6 +21,8 @@ def meetup_date(year: int, month: int, nth: int = 4, weekday: int = 3) -> dateti
         month: month when the meetup will be held
         nth: the nth occurence of weekday in the given year/month to use as the meetup date
             example: 2 = 2nd occurence, 1 = first occurence
+            example: -1 = last occurence in the month, -2 = 2nd to last occurence in the month
+            example: 0 = not acceptable input
         weekday: the int weekday value, where 0=Monday and 6=Sunday
             example: 4 = Friday
     Returns:
@@ -29,16 +31,26 @@ def meetup_date(year: int, month: int, nth: int = 4, weekday: int = 3) -> dateti
         meetup_date(2015, 8, 4, 3) returns datetime.date(2015, 8, 27) as
         the date that is the 4th Thursday (3=Thurs) in August of 2015.
     """
-    # get the day of the week for the first of the year/month
-    weekday_of_the_first = datetime.date(year, month, 1).weekday()
+    # check args
+    if nth is 0:
+        raise ValueError("nth arg cannot be 0")
+
+    # get an anchor date, being the first of the current or next month, to add or subtract
+    # days to/from to get the meetup date
+    anchor_month = month if nth > 0 else month + 1
+    anchor_year = year
+    if anchor_month > 12:
+        anchor_month = 1
+        anchor_year += 1
+    anchor_date = datetime.date(anchor_year, anchor_month, 1)
 
     # get days offset from the first to get the nth occurrence of weekday
-    first_weekday_occurrence_offset = (weekday - weekday_of_the_first) % 7
-    nth_offset = 7 * (nth - 1) # number of days to add to date to get the nth occurence of weekday
+    first_weekday_occurrence_offset = (weekday - anchor_date.weekday()) % 7
+    nth_offset = 7 * (nth - 1) if nth > 0 else 7*nth
 
-    # get the date of the month
-    target_day_of_month = 1 + first_weekday_occurrence_offset + nth_offset # first of the month + offsets
-    date_of_meetup = datetime.date(year,month, target_day_of_month)
+    # get the date of the month by moving days_offset days from the anchor date
+    days_offset = first_weekday_occurrence_offset + nth_offset
+    date_of_meetup = anchor_date + datetime.timedelta(days=days_offset)
 
     return date_of_meetup
 
